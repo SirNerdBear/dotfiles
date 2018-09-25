1d625860101c605b6ded5547bfaa16dc8aee21c2
Scott Hall <scott@codezoom.com>
2018-09-25 12:54:40 -0700

    add history plugins to zsh

--- .zshrc
+++ .zshrc
@@ -60,7 +60,7 @@ SPACESHIP_BATTERY_SHOW=false
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git gpg)
+plugins=(git gpg zsh-autosuggestions history-substring-search)
 
 source $ZSH/oh-my-zsh.sh
 
--- install.rb
+++ install.rb
@@ -225,6 +225,8 @@ end
 
 #install Oh My ZSH
 system "sh", "-c", `curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh`
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 
 
 
