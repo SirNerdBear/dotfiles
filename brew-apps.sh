812d0adbddb71fab634f93a7bc3905b9e1132d17
Scott Hall <scott@codezoom.com>
2018-09-27 22:23:55 -0700

    add java and arduino ide to cask installs

--- brew-apps.sh
+++ brew-apps.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-#AppStore
+#AppStore https://github.com/mas-cli/mas
 #mas signin --dialog #NEED EMAIL#
 mas install 409203825 #Numbers
 mas install 409203825 #XCoode
@@ -10,22 +10,12 @@ mas install 409201541 #The Archive Browser
 mas install 409183694 #Keynote
 mas install 425424353 #The Unarchiver
 
+
+brew cask install java
+brew cask install arduino
 # Install apps
 
-# Chrome & Canary
 # Photoshop
-# Alfred
-# BetterTouchTool
-# Discord
-# iTerm2
-# Docker
-# Arduino,
-
-# #https://github.com/mas-cli/mas
-# CLI App: Xcode, Pages, Numbers, Keynote, The Archive Browser
-
-#For the most part I should switch to Docker instead of dealing with DB
-#instances on my local machine, etc. 
 
 # Install main applications
 brew cask install iterm2
@@ -48,7 +38,6 @@ brew install homebrew/completions/docker-completion
 brew install homebrew/completions/docker-compose-completion
 
 brew tap caskroom/cask
-#brew cask install atom
 brew cask install docker
 brew cask install dropbox
 brew cask install firefox
@@ -59,7 +48,6 @@ brew cask install kindle
 brew cask install monolingual
 brew cask install sketch
 brew cask install sketch-toolbox
-#brew cask install skype
 brew cask install soulver
 brew cask install transmit
 brew cask install vlc
