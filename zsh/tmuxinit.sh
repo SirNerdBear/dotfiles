9920400d95197327f67f08d89fc91b8d3aafaee1
Scott Hall <scott@codezoom.com>
2018-09-28 21:28:51 -0700

    move test.py to finished tmuxstatus and fix a bug

---
+++ zsh/tmuxinit.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+if [ -f /tmp/powerline-ipc-`id -u`.pid ]; then
+    if ps -p $(cat /tmp/powerline-ipc-`id -u`.pid) -o command= | grep -q 'tmuxstatus'; then
+        exit 0;
+    fi
+fi
+
+~/.config/zsh/tmuxstatus
