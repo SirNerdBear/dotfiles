bf419248004aaca3fc9dd52ccf87809a6479df6b
Scott Hall <scott@codezoom.com>
2018-10-05 21:57:03 -0700

    refactor

--- tmuxinator/tmuxinit.sh
+++ tmuxinator/tmuxinit.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env zsh
-if [ -f /tmp/powerline-ipc-`id -u`.pid ]; then
-    if ps -p $(cat /tmp/powerline-ipc-`id -u`.pid) -o command= | grep -q 'tmuxstatus'; then
+if [ -f /tmp/tmuxstatus-ipc-`id -u`.pid ]; then
+    if ps -p $(cat /tmp/tmuxstatus-ipc-`id -u`.pid) -o command= | grep -q 'tmuxstatus'; then
         exit 0;
     fi
 fi
 
-~/.config/tmux/tmuxstatus
+~/.config/tmuxinator/tmuxstatus
