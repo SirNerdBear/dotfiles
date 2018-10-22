db56a372f21aab5b14e0cfa4ad32d4f18e29fbf5
Scott D Hall <scott@codezoom.com>
2018-10-22 08:03:29 -0700

    add prompt configs to pythonstartup

--- python/pythonstartup.py
+++ python/pythonstartup.py
@@ -1,10 +1,45 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import atexit
 import os
 import errno
 import readline
+import sys
+#import rlcompleter
 
+from pprint import pprint
+from __future__ import print_function
+
+import prettyprinter
+prettyprinter.install_extras(["python"])
+prettyprinter.set_default_style("dark")
+from prettyprinter import cpprint
+
+
+#from tempfile import mkstemp
+#from code import InteractiveConsole
+#import datetime
+#import pdb
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import Terminal256Formatter
+
+#if sys.version_info.major == 3:
+
+sys.ps1 = "\033[0;38;5;32;48;5;237m \033[49;38;5;237m\033[0m "
+sys.ps2 = "\033[0;38;5;32;48;5;237m …\033[49;38;5;237m\033[0m "
+
+def my_displayhook(value):
+    #if value is not None:
+    try:
+        import __builtin__
+        __builtin__._ = value
+    except ImportError:
+        __builtins__._ = value
+    sys.stdout.write('=> ')
+    cpprint( value )
+sys.displayhook = my_displayhook
 
 data_dir = os.path.join(os.path.expanduser("~"), ".local/share/python")
 try:
