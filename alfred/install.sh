dc022572c030561e574878eb035b76212b207077
Scott Hall <scott@nerdhq.net>
2025-01-16 21:01:13 -0800

    Alfred 5

--- alfred/install.sh
+++ alfred/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 defaults write com.runningwithcrayons.Alfred-Preferences syncfolder "${HOME}/dotfiles/alfred"
-! $(pgrep -q "Alfred 5") && open /Applications/Alfred\ 4.app
+! $(pgrep -q "Alfred 5") && open /Applications/Alfred\ 5.app
 
