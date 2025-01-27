c5b2145517de33f3c99051907c9153f8f5988f31
Scott Hall <scott@nerdhq.net>
2025-01-26 21:40:35 -0800

    Add XDG for Go

--- ansible/roles/dev_cli/files/exports.zsh
+++ ansible/roles/dev_cli/files/exports.zsh
@@ -13,6 +13,7 @@ export PYENV_ROOT="$XDG_DATA_HOME"/pyenv
 export PYTHONSTARTUP="$XDG_CONFIG_HOME"/python/pythonrc
 export RUSTUP_HOME="$XDG_DATA_HOME"/rustup
 export CARGO_HOME="$XDG_DATA_HOME"/cargo
+export GOPATH="$XDG_DATA_HOME"/go
 
 
 export HISTFILE="$XDG_STATE_HOME/zsh/history"
